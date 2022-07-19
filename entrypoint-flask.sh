ed263763ce9357c42834cc8f06d6ec0afdd13d46
Bruno Teruya <bruno.teruya@lb2.com.br>
2022-07-19 16:36:33 -0300

    Adequando

--- entrypoint-flask.sh
+++ entrypoint-flask.sh
@@ -4,8 +4,7 @@ PYTHON_VERSION=3.6
 
 if [[ -f requirements.txt ]]; then
 	echo Instalando PIPINSTALL
-    pip3 install -r requirements.txt
+    pip install -r requirements.txt
 fi
 
-exec /opt/python${PYTHON_VERSION}/bin/pyinstaller $@ --hidden-import=flask
-# --hidden-import=flask_api.parsers --hidden-import=flask_api.renderers
+exec /opt/python${PYTHON_VERSION}/bin/pyinstaller $@ --hidden-import=flask_api --hidden-import=flask_api.parsers --hidden-import=flask_api.renderers
