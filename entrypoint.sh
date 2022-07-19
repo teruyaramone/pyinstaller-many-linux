86da0a976520898a8d51a396aead0332e876ec02
Bruno Teruya <bruno.teruya@lb2.com.br>
2022-07-19 15:04:55 -0300

    Alterado pip do branch 3.6

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ PYTHON_VERSION=3.6
 
 if [[ -f requirements.txt ]]; then
 	echo Instalando PIPINSTALL
-    pip2.7 install -r requirements.txt
+    pip install -r requirements.txt
 fi
 
 exec /opt/python${PYTHON_VERSION}/bin/pyinstaller $@
